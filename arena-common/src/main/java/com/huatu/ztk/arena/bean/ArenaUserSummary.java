12b927a87cc53963b269afbc489b0f9881cfe74a
shaojieyue <yuesj@huatu.com>
2016-10-17 16:48:19 +0800

    完善代码

--- arena-common/src/main/java/com/huatu/ztk/arena/bean/ArenaUserSummary.java
+++ arena-common/src/main/java/com/huatu/ztk/arena/bean/ArenaUserSummary.java
@@ -1,9 +1,9 @@
 package com.huatu.ztk.arena.bean;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
  * 用户竞技场统计
@@ -15,7 +15,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(force = true)
 @AllArgsConstructor
 @Builder
+@Document(collection = "ztk_arena_summary")
 public class ArenaUserSummary {
+    @Id
+    @Getter(onMethod = @__({ @JsonIgnore}))
+    private String id;//id结构 uid+yyyyMMdd
     private long uid;//用户id
     private int winCount;//胜利场次
     private int failCount;//失败场次
